8c39bc1dde05acaf99c3cf74ebb68608d2262f88
Anthony RDutra <97138694+AnthonyDRdutra@users.noreply.github.com>
2023-11-15 17:48:31 -0300

    Update applychanges.php

--- crudphp/Include/applychanges.php
+++ crudphp/Include/applychanges.php
@@ -1,12 +1,14 @@
 <?php
 include "CRUD_logic.php";
-
+//we call our classes
 $update = new \Include\CRUD_logic();
 $apply = new \Include\CRUD_logic();
 
+//got our inputs
 $id = filter_input(INPUT_POST, 'id');
 $input_name = filter_input(INPUT_POST, 'nome');
 $input_email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
 
+//we sent our inputs to their functions
 $update->Update_client($id);
 $apply ->apply_changes($id,$input_name,$input_email);
